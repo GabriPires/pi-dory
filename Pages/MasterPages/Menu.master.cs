21815563d3311441ab3aff8f292c2851b7692e2c
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-17 19:20:56 -0300

    Erro botão logout corrigido

--- Pages/MasterPages/Menu.master.cs
+++ Pages/MasterPages/Menu.master.cs
@@ -22,17 +22,21 @@ public partial class Pages_MasterPages_Menu : System.Web.UI.MasterPage
         else
         {
             ltlTextoLogado.Text =  "<li class='list-inline-item text'>";
-            ltlTextoLogado.Text += "<p>" + Session["nomeResponsavel"].ToString() + "</p>";
+            ltlTextoLogado.Text += "<a href='#' class='list-inline-item btn-entrarCadastrar'>" + Session["nomeResponsavel"].ToString() + "</a>";
             ltlTextoLogado.Text += "</li>";
-            ltlTextoLogado.Text += "<li class='list-inline-item'>|</li>";
+            ltlTextoLogado.Text += "<li class='list-inline-item mr-0'>|</li>";
             ltlTextoLogado.Text += "<li class='list-inline-item'>";
-            ltlTextoLogado.Text += "<a href='CadastroInicialResponsavel.aspx' class='mb-0 btn-entrarCadastrar'>Logout</a>";
+            ltlTextoLogado.Text += "<asp:Button ID='btnLogout' runat='server' CssClass='mb-0 btn-entrarCadastrar' Text='Logout' OnClick='btnLogout_Click' />";
             ltlTextoLogado.Text += "</li>";
         }
     }
 
     protected void btnLogout_Click(object sender, EventArgs e)
     {
-
+        Session.Remove("nomeResponsavel");
+        Session.Remove("emailResponsavel");
+        Session.Remove("idResponsavel");
+        Session.Remove("idPessoa");
+        Response.Redirect("Index.aspx");
     }
 }
