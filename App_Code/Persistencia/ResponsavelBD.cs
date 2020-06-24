1ff864b4a13c566ddaf1c6ea62e097cdaa8e7dcf
Matheus <matheusrodrigofilho@outlook.com>
2020-06-24 10:38:41 -0300

    Limpando o codigo

--- App_Code/Persistencia/ResponsavelBD.cs
+++ App_Code/Persistencia/ResponsavelBD.cs
@@ -51,65 +51,9 @@ public class ResponsavelBD
         return retorno;
     }
 
-    public static int InsertResponsavel(Responsaveis responsavel)
-    {
-        int retorno = 0;
-
-        try
-        {
-            IDbConnection objConnection;
-            IDbCommand objCommand;
-
-            
-            string sql = "INSERT INTO res_responsaveis(res_email, res_senha,pes_id) VALUES(?res_email,?res_senha, ?pes_id); ";
 
 
-            objConnection = Mapped.Connection();
-            objCommand = Mapped.Command(sql, objConnection);
-
-            //parametrização
-            objCommand.Parameters.Add(Mapped.Parameter("?res_email", responsavel.Res_email));
-            objCommand.Parameters.Add(Mapped.Parameter("?res_senha", responsavel.Res_senha));
-
-            objCommand.Parameters.Add(Mapped.Parameter("?pes_id", responsavel.Pes_id.Pes_id));
-
-            objCommand.ExecuteNonQuery();
-
-            objConnection.Close();
-            objConnection.Dispose();
-            objCommand.Dispose();
-        }
-        catch (Exception ex)
-        {
-            retorno = -2;
-        }
-        return retorno;
-    }
-
-    public static DataSet SelectID(Pessoas pessoa)
-    {
-        DataSet ds = new DataSet();
-        IDbConnection objConnection;
-        IDbCommand objCommand;
-        IDataAdapter objDataDadapter;
-
-        objConnection = Mapped.Connection();
-
-        
-        string sql = "SELECT pes_id FROM pes_pessoas WHERE pes_nome = ?pes_nome";
-        //objCommand    = Mapped.Command("select * from perfil", objConnection);
-        objCommand = Mapped.Command(sql, objConnection);
-
-        objCommand.Parameters.Add(Mapped.Parameter("?pes_nome", pessoa.Pes_nome));
-
-        objDataDadapter = Mapped.Adapter(objCommand);
-        objDataDadapter.Fill(ds);
-
-        objConnection.Close();
-        objCommand.Dispose();
-        objConnection.Dispose();
-        return ds;
-    }
+    
 
     public static DataSet SelectLogin(string email, string senha)
     {
